8403c738b8b9da3852204805bdb6fd009c5397e7
Pavel Petrov <doctorrosen2016@gmail.com>
2025-03-27 00:03:01 +0600

    prepare to styling of project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
+import { FirstComponent } from './components/first/first.component';
 
 export const routes: Routes = [
   {
